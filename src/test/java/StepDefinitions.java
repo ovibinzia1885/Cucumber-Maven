b6e2a2b4e7db71fe41da6ae3db14b4f8e3dcf3cf
ovibinzia1885 <nazmul35-1885@diu.edu.bd>
2022-01-03 21:42:55 +0600

    uploaded

---
+++ src/test/java/StepDefinitions.java
@@ -0,0 +1,24 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class StepDefinitions {
+    @Given("User visits e-commerce website")
+    public void user_visits_e_commerce_website() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @When("User enters valid credentials")
+    public void user_enters_valid_credentials() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+    @Then("User can logged in successfully")
+    public void user_can_logged_in_successfully() {
+        // Write code here that turns the phrase above into concrete actions
+
+    }
+
+
+
+}
