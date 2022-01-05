78b5b49bd849d7393b954841f83e2e67338c0ba2
ovibinzia1885 <nazmul35-1885@diu.edu.bd>
2022-01-05 21:46:46 +0600

    uploaded

--- src/test/java/StepDefinitions.java
+++ src/test/java/StepDefinitions.java
@@ -1,11 +1,24 @@
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.concurrent.TimeUnit;
 
 public class StepDefinitions {
+    public WebDriver driver;
     @Given("User visits e-commerce website")
     public void user_visits_e_commerce_website() {
-        // Write code here that turns the phrase above into concrete actions
+        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+        ChromeOptions ops = new ChromeOptions();
+        ops.addArguments("--headed"); //uncomment if you want to run in headless mode
+        driver = new ChromeDriver(ops);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("http://automationpractice.com");
+
 
     }
     @When("User enters valid credentials")
